4e3b2b152c464663199ff6493c937c649d50b519
Vlad Sevruk <vladworking5@gmail.com>
2024-10-03 11:12:14 +0300

    3

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-day = int(input("Введите день: "))
-month = int(input("Введите месяц: "))
+day = int(input("Введите день: "))# ввод дня
+month = int(input("Введите месяц: ")) # ввод месяца
+if month<1 or month>12: # Проверяем на корректный месяц.
+
+    print("Ошибка месяца") # Выводим что у нас некорректный месяц.
+
+elif day<1 or day>31: # Проверяем на корректный день.
+    
+    print ("Некорректный день") # Выводим что у нас некорректный день.
+    
+else:# Если day и month валидны, выполниться этот блок.
 if (month == 3 and day >= 1) or (month == 4) or (month == 5 and day <= 31):
     season = "Весна"
 elif (month == 6 and day >= 1) or (month == 7) or (month == 8) or (month == 8 and day <= 31):
